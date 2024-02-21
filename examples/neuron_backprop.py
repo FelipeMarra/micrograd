b75c7681fca2963ec3434e3290c7853e1eedf791
Felipe Marra <felipeferreiramarra@aluno.ufsj.edu.br>
2024-02-21 14:57:29 -0300

    auto backprop, corrention for cumulative gradients & number * Value

--- examples/neuron_backprop.py
+++ examples/neuron_backprop.py
@@ -5,6 +5,41 @@ sys.path.append(os.path.pardir)
 
 from micrograd.engine import *
 from micrograd.viz import *
+
 #%%
 # BACKPROPAGATION FOR A NEURON:
 # inputs x1, x2
+x1 = Value(2.0, label='x1')
+x2 = Value(0.0, label='x2')
+#wights w1, w2
+w1 = Value(-3, label='w1')
+w2 = Value(1.0, label='w2')
+#bias 
+b = Value(6.8813735870195432, label='b')
+#x1w1 + x2w2 + b
+x1w1 = x1 * w1; x1w1._label='x1w1'
+x2w2 = x2 * w2; x2w2._label='x2w2'
+x1w1x2w2 = x1w1 + x2w2; x1w1x2w2._label='x1w1 + x2w2'
+n = x1w1x2w2 + b; n._label='n'
+o = n.tanh(); o._label = 'out'
+draw_dot(o)
+
+# %%
+# Manual Backprop
+o.grad = 1.0
+o._backward()
+n._backward()
+b._backward()
+x1w1x2w2._backward()
+x2w2._backward()
+x1w1._backward()
+draw_dot(o)
+
+# %%
+# Auto Backprop
+o.backward()
+draw_dot(o)
+
+#%%
+2 + Value(2)
+# %%
--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,3 +1,6 @@
+#%%
+import math
+
 #%% 
 class Value:
     """
@@ -22,6 +25,7 @@ class Value:
     def __init__(self, data:float, prev:tuple=(), op='', label=''):
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = set(prev)
         self._op = op
         self._label = label
@@ -38,12 +42,71 @@ class Value:
             The logic will be the same for every other 
             operator
         """
+        other = other if isinstance(other, Value) else Value(other)
         data = self.data + other.data
         prev = (self, other)
-        return Value(data, prev, '+')
+        out = Value(data, prev, '+')
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+        return out 
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         data = self.data * other.data
         prev = (self, other)
-        return Value(data, prev, '*')
+        out = Value(data, prev, '*')
+
+        def _backward():
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
+
+        out._backward = _backward
+
+        return out
+    
+    def __rmul__(self, other):
+        """
+            This function is called in cases like 2 * a,
+            where 2 doesn't know how to be multiplied by the Value a.
+            Here we are inverting the operation to a * 2, 
+            so that a__mul__(2) will be executed instead.
+        """
+        return self * other
+    
+    def tanh(self):
+        x = self.data
+        # e^(2x) -1 / e^(2x) +1
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self,), 'tanh')
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        # Topological Sorting: wikipedia.org/wiki/Topological_sorting
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for prev in v._prev:  
+                    build_topo(prev)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
+        self.grad = 1
 # %%
